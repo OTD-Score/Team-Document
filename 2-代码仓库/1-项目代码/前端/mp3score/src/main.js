6b3de73fa3c91aef055e2f404f2d817bd607cbe9
isryds <145736903+isryds@users.noreply.github.com>
2024-02-26 21:23:07 +0800

    Update main.js

--- 2-代码仓库/1-项目代码/前端/mp3score/src/main.js
+++ 2-代码仓库/1-项目代码/前端/mp3score/src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Router from '@/router/index'
 import axios from 'axios';
+/*引入自动适应不同分辨率配置*/
+import './util/rem'
 
 
 const app = createApp(App);
